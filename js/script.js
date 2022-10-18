2a247aecb873eefaee57c7c01ada411edf6f8d00
SiddharthB70 <siddharth7002b@gmail.com>
2022-10-18 21:29:45 +0530

    Update Game Board and Display

--- js/script.js
+++ js/script.js
@@ -1,21 +1,55 @@
-const displayBoard = (function(){
-    const cells = document.querySelectorAll(".gameboard td");
-    function returnCells(){
-        return cells
+const gameBoard = (function(){
+    const board =  [["","",""],
+                    ["","",""],
+                    ["","",""]];
+    const playerSymbol = {
+        "player1": "X",
+        "player2": "O"
+    };
+    let presentPlayer = "player1";
+    let presentSymbol = "X"
+    const xCoordinate = 0, yCoordinate = 1;
+
+    function updateGameBoard(cell){
+        board[cell[xCoordinate]][cell[yCoordinate]] = presentSymbol;
     }
 
+    function getPresentSymbol(){
+        return presentSymbol;
+    }
+
+    return {updateGameBoard,getPresentSymbol};
+})();
+
+const displayController = (function(){
+    const boardCells = document.querySelectorAll(".gameboard td");   
     function cellClick(){
-        cells.forEach((cell)=>{
-            cell.addEventListener("click",addX);
+        boardCells.forEach((cell)=>{
+            cell.addEventListener("click",selectCell);
         })
     }
-    
-    function addX(e){
-        e.target.textContent = "X";
+
+    function selectCell(e){
+        const cell = e.target;
+        cell.removeEventListener("click",selectCell);
+        cell.classList.remove("hover");
+        gameBoard.updateGameBoard(returnCoordinate(cell));
+        updateDisplay(cell);
+    }
+
+    function returnCoordinate(cell){
+        let y = cell.getAttribute("data-column");
+        const row = cell.closest("tr");
+        let x = row.getAttribute("data-row");
+        return [x,y];
+    }
+
+    function updateDisplay(cell){
+        cell.textContent = gameBoard.getPresentSymbol();
     }
 
     return {cellClick};
 })();
 
-displayBoard.cellClick();
+displayController.cellClick();
 
