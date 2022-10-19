1980d0278cbfd1a21107c95b17e27447bda012d5
SiddharthB70 <siddharth7002b@gmail.com>
2022-10-19 16:44:28 +0530

    Win Logic

--- js/script.js
+++ js/script.js
@@ -29,7 +29,11 @@ const gameBoard = (function(){
         presentSymbol = symbol;
     }
 
-    return {updateGameBoard,getPresentSymbol,getPresentPlayer,updatePresentPlayer};
+    function getCellValue(cellCoordinate){
+        return board[cellCoordinate[xCoordinate]][cellCoordinate[yCoordinate]];
+    }
+
+    return {updateGameBoard,getPresentSymbol,getPresentPlayer,updatePresentPlayer,getCellValue};
 })();
 
 /*
@@ -48,10 +52,17 @@ const displayController = (function(){
 
     function selectCell(e){
         const cell = e.target;
+        const cellCoordinate = returnCoordinate(cell);
+
+        //To update Displayed cell
         cell.removeEventListener("click",selectCell);
         cell.classList.remove("hover");
-        gameBoard.updateGameBoard(returnCoordinate(cell));
         updateDisplay(cell);
+
+        //To update Array cell
+        gameBoard.updateGameBoard(cellCoordinate);
+
+        flowControl.moveControl(cellCoordinate);
     }
 
     function returnCoordinate(cell){
@@ -94,12 +105,76 @@ const player = (playerName,playerSymbol)=>{
     }
 
     return {setPlayerDetails,getPlayerName,getPlayerSymbol,updatePlayerName,updatePlayerSymbol};
-    
 };
 
 
 const flowControl = (function(){
+    const xCoordinate  = 0, yCoordinate = 1;
 
+    function moveControl(cellCoordinate){
+        let win = checkWin(cellCoordinate);
+        if(!win)
+            switchPlayer();
+    }
+
+    function checkWin(cellCoordinate){
+        const presentSymbol = gameBoard.getPresentSymbol();
+        let cWin = checkColumn(cellCoordinate[yCoordinate],presentSymbol);
+        let rWin = checkRow(cellCoordinate[xCoordinate],presentSymbol);
+        let dWin;
+        if(cellCoordinate[xCoordinate] == cellCoordinate[yCoordinate])
+            dWin = checkLeftDiagonal(presentSymbol);
+        if(Math.abs(cellCoordinate[xCoordinate]-cellCoordinate[yCoordinate]) == 2)
+            dWin = checkRightDiagonal(presentSymbol)
+        return cWin||rWin||dWin;
+    }
+
+    function checkColumn(column,symbol){
+        for(let x=0;x<=2;x++){
+            if(gameBoard.getCellValue([x,Number(column)])!=symbol)
+                return false;
+        }
+        return true;
+    }
+
+    function checkRow(row,symbol){
+        for(let y=0;y<=2;y++){
+            if(gameBoard.getCellValue([Number(row),y])!=symbol)
+                return false;
+        }
+        return true;
+    }
+
+    function checkLeftDiagonal(symbol){
+        let x = 0,y = 0;
+        while(x<=2 && y<=2){
+            if(gameBoard.getCellValue([x,y])!=symbol)
+                return false;
+            x++,y++;
+        }
+        return true;
+    }
+
+    function checkRightDiagonal(symbol){
+        let x = 0, y = 2
+        while(x<=2 && y>=0){
+            if(gameBoard.getCellValue([x,y])!=symbol)
+                return false;
+            x++,y--;
+        }
+        return true;
+    }
+
+    function switchPlayer(){
+        presentPlayer = gameBoard.getPresentPlayer();
+        if(presentPlayer === player1)
+            gameBoard.updatePresentPlayer(player2,player2.getPlayerSymbol());
+        
+        else    
+            gameBoard.updatePresentPlayer(player1,player1.getPlayerSymbol());
+    }
+
+    return {moveControl};
 })();
 
 const player1 = player("Player 1","X");
@@ -107,5 +182,6 @@ const player2 = player("Player 2","O");
 
 window.onload = () =>{
     displayController.cellClick();
-    gameBoard.updatePresentPlayer(player1.getPlayerName(),player1.getPlayerSymbol());
+    gameBoard.updatePresentPlayer(player1,player1.getPlayerSymbol());
 };
+
